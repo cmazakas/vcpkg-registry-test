94738801fc83489805abb0698d748d2cf139ffc8
Christian Mazakas <christian.mazakas@gmail.com>
2025-04-04 11:25:21 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Boost.Bloom Demo
+
+Make sure vcpkg is installed.
+
+Make a custom toolchain file similar to:
+```cmake
+# toolchain.cmake
+cmake_policy(SET CMP0167 NEW) # for FindBoost.cmake deprecation warnings
+include(/home/exbigboss/cpp/vcpkg/scripts/buildsystems/vcpkg.cmake)
+```
+
+Build the demo!
+
+```bash
+cmake -B __build -S . -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake
+cmake --build __build/
+./__build/registry-test
+```
---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <boost/bloom/filter.hpp>
+#include <cassert>
+#include <string>
+#include <iostream>
+
+int main()
+{
+  std::cout << "Thank you for trying the demo of Boost.Bloom!" << std::endl;
+
+  // Bloom filter of strings with 5 bits set per insertion
+  using filter = boost::bloom::filter<std::string, 5>;
+
+  // create filter with a capacity of 1'000'000 **bits**
+  filter f(1'000'000);
+
+  // insert elements (they can't be erased, Bloom filters are insert-only)
+  f.insert("hello");
+  f.insert("Boost");
+  //...
+
+  // elements inserted are always correctly checked as such
+  assert(f.may_contain("hello") == true);
+
+  // elements not inserted may incorrectly be identified as such with a
+  // false positive rate (FPR) which is a function of the array capacity,
+  // the number of bits set per element and generally how the boost::bloom::filter
+  // was specified
+  if(f.may_contain("bye")) { // likely false
+    //...
+  }
+}
